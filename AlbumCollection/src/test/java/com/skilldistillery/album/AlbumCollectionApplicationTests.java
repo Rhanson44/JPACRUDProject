6e674ee547cf8114dbaeebc23169b7b919a9db13
Ryan Hanson <hansonrj8322@gmail.com>
2024-08-16 16:44:23 -0600

    create bootmvc project, connect JPA dependency

---
+++ AlbumCollection/src/test/java/com/skilldistillery/album/AlbumCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.album;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
